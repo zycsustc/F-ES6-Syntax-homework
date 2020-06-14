0d939ac0814bef3e2db0f537a44296b19d2a7864
yichi zhang <yichi.zhang@thoughtworks.com>
2020-06-14 22:23:26 +0800

    [zycsustc]-done

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = function (input) {
+    let result = [];
+    for(let data of input.data){
+        let person = {};
+        let index = 0;
+        for(let column of input.column){
+            person[column.name] = data[index];
+            index += 1;
+        }
+        result.push(person);
+    }
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = function (items, sections) {
+    for(let i=0;i<sections.length;i++){
+        let selfIndex = sections[i].index+i;
+        items.splice(selfIndex, 0, sections[i].content);
+    }
+    return items;
+}
 export { inject };
